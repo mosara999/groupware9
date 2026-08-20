577e025d6a0bec15091c50044ab1bce4f2638790
mosara999 <mosara999@hanmail.net>
2026-08-20 18:02:06 +0900

    백필 스크립트의 재귀 제한 변수명 수정 (MariaDB용)
    MySQL의 cte_max_recursion_depth는 MariaDB에 없는 변수라 실행 시 에러 발생.
    MariaDB 10.11에서는 max_recursive_iterations를 사용해야 함.

--- backfillComDate.sql
+++ backfillComDate.sql
@@ -2,7 +2,7 @@
 -- 월간 일정(schList) 그리드가 그 범위 밖의 달(예: 2026-08)에서는 날짜가 하나도 표시되지 않는 문제 해결용.
 -- 2021-01-01 ~ 2035-12-31 구간을 채운다. 이미 존재하는 날짜는 건너뛴다.
 
-SET SESSION cte_max_recursion_depth = 10000;
+SET SESSION max_recursive_iterations = 10000;
 
 INSERT INTO COM_DATE (CDDATE, CDYEAR, CDMM, CDDD, CDWEEKOFYEAR, CDWEEKOFMONTH, CDWEEK, CDDAYOFWEEK)
 WITH RECURSIVE seq AS (
